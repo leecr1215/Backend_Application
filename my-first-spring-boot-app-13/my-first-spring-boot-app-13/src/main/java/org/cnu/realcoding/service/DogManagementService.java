61c45a467804d3b2af79e42f1307b3ddb7610bb7
Juyeong-Lee <lge2515@naver.com>
2021-04-07 02:08:05 +0900

    Update DogManagementService.java

--- my-first-spring-boot-app-13/my-first-spring-boot-app-13/src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ my-first-spring-boot-app-13/my-first-spring-boot-app-13/src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -26,7 +26,13 @@ public class DogManagementService {
         }
         throw new DogNotFoundException();
     }
+<<<<<<< Updated upstream
     public Dog DogByOwnerName(String ownerName){
+=======
+
+    public Dog getDogByOwnerName(String ownerName){
+        dogs = dogRepository.findDogByOwnerName(ownerName);
+>>>>>>> Stashed changes
         for (Dog dog : dogs) {
             if (dog.getOwnerName().equals(ownerName)) {
                 return dog;
@@ -35,24 +41,40 @@ public class DogManagementService {
         throw new DogNotFoundException();
     }
 
+<<<<<<< Updated upstream
     public Dog DogByPhoneNumber(String phoneNum){
         for (Dog dog : dogs) {
             if (dog.getOwnerPhoneNumber().equals(phoneNum)) {
+=======
+    public Dog getDogByPhoneNumber(String ownerPhoneNumber){
+        dogs = dogRepository.findDogByPhoneNumber(ownerPhoneNumber);
+        for (Dog dog : dogs) {
+            if (dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+>>>>>>> Stashed changes
                 return dog;
             }
         }
         throw new DogNotFoundException();
     }
 
+<<<<<<< Updated upstream
     public Dog DogByAll(String dogName, String ownerName, String phoneNum){
         for (Dog dog : dogs) {
             if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(phoneNum)) {
+=======
+    public Dog getDogByAll(String dogName, String ownerName, String ownerPhoneNumber){
+        dogs = dogRepository.findDogByAll(dogName,ownerName,ownerPhoneNumber);
+
+        for (Dog dog : dogs) {
+            if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+>>>>>>> Stashed changes
                 return dog;
             }
         }
         throw new DogNotFoundException();
     }
 
+<<<<<<< Updated upstream
     public void UpdateAllInfo(String dogName, String ownerName, String phoneNum, String newDogName, String newDogKind,String newOwnerName, String newPhoneNum){
         for (Dog dog : dogs) {
             if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(phoneNum)) {
@@ -79,6 +101,56 @@ public class DogManagementService {
             if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(phoneNum)) {
                 dog.getMedidcalRecords().add(medicalRecord);
             }
+=======
+    public void UpdateAllInfo(String dogName, String ownerName, String ownerPhoneNumber, String newDogName, String newDogKind,String newOwnerName, String newPhoneNum){
+        dogs = dogRepository.findAllDog();
+        try {
+            for (Dog dog : dogs) {
+                if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                    dog.setName(newDogName);
+                    dog.setKind(newDogKind);
+                    dog.setOwnerName(newOwnerName);
+                    dog.setOwnerPhoneNumber(newPhoneNum);
+                    dogRepository.updateAll(dogName, ownerName, ownerPhoneNumber, newDogName, newDogKind, newOwnerName, newPhoneNum);
+                }
+            }
+        } catch(DogNotFoundException e) {
+
+        } finally {
+            dogs = dogRepository.findAllDog();
+        }
+    }
+
+    public void UpdateKind(String dogName, String ownerName, String ownerPhoneNumber, String newDogKind){
+        dogs = dogRepository.findAllDog();
+        try{
+            for (Dog dog : dogs) {
+                if (dog.getName().equals(dogName) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)) {
+                    dog.setKind(newDogKind);
+                    dogRepository.updateKind(dogName,newDogKind,ownerName,ownerPhoneNumber);
+                }
+            }
+        }catch (DogNotFoundException e){
+
+        } finally {
+            dogs = dogRepository.findAllDog();
+        }
+
+    }
+
+    public void InsertMedicalRecord(String name, String ownerName, String ownerPhoneNumber, String newMedicalRecord){
+        dogs = dogRepository.findAllDog();
+        try {
+            for (Dog dog : dogs) {
+                if (dog.getName().equals(name) && dog.getOwnerName().equals(ownerName) && dog.getOwnerPhoneNumber().equals(ownerPhoneNumber)){
+                    dogRepository.insertMedicalRecords(name, ownerName, ownerPhoneNumber, newMedicalRecord);
+                }
+            }
+        } catch (DogNotFoundException e) {
+
+        } finally {
+            dogs = dogRepository.findAllDog();
+>>>>>>> Stashed changes
         }
     }
 }
